e8e15cd813b92aac65240663a7958905c930efdd
RazzaqShikalgar <ayubshikalgar2@gmail.com>
2024-04-22 23:24:18 +0530

    debugging done

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_5v$cgx#c*%6li-q&r2=$@pta%sjmb7tlb7+fb2(4kz1d&@e(2"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = ['http://localhost', 'https://localhost']
+CSRF_COOKIE_SECURE = False
+
 
 
 # Application definition
